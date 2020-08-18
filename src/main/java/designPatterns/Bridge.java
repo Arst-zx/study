dcc1752c6b8c22c3281452648c5373832a8de01c
zhangx511 <zhangx511@chinaunicom.cn>
2020-08-18 08:53:53 +0800

    桥接模式和装饰模式

---
+++ src/main/java/designPatterns/Bridge.java
@@ -0,0 +1,13 @@
+package designPatterns;
+
+/**
+ * @author: 章鑫
+ * @Email: zhangx511@chinaunicom.cn
+ * @Project_name：Study
+ * @Name: Bridge
+ * @date: 2019-10-09 10:19
+ * @Description: 桥接模式
+ **/
+
+public class Bridge {
+}
