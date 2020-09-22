c9c588fbe4bfa0038f8b626d5088f20f91d09e9d
zhangx511 <zhangx511@chinaunicom.cn>
2020-09-22 16:09:22 +0800

    leetcode算法

--- src/main/java/algorithm/java/Solution1.java
+++ src/main/java/algorithm/java/Solution1.java
@@ -9,21 +9,6 @@ package algorithm.java;
  * @Description: 两数之和
  **/
 
-/**
- * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
- *
- * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
- *
- * 示例:
- *
- * 给定 nums = [2, 7, 11, 15], target = 9
- *
- * 因为 nums[0] + nums[1] = 2 + 7 = 9
- * 所以返回 [0, 1]
- *
- * Related Topics 数组 哈希表
- */
-
 public class Solution1 {
     public int[] twoSum(int[] nums, int target) {
 
@@ -70,6 +55,9 @@ class Solution {
     }
 }
 
+/**
+ * 整数反转
+ */
 class Solutionl {
     public int reverse(int x) {
         if (x >= Integer.MAX_VALUE || x <= Integer.MIN_VALUE) {
@@ -98,6 +86,43 @@ class Solutionl {
     public static void main(String[] args) {
         Solutionl solutionl = new Solutionl();
         int result = solutionl.reverse(0);
+    }
+}
+
+class Solution12 {
+    public boolean isPalindrome(int x) {
+        if (x >=0) {
+            int[] result = new int[32];
+            int shang = x;
+            int i = 0;
+            while (shang != 0) {
+                int yushu = shang%10;
+                shang = shang/10;
+                result[i] = yushu;
+                i++;
+            }
+
+            long sum = 0;
+            for (int m=0; m<i; m++) {
+                Double pow = Math.pow(10d, i - m - 1);
+                sum = (long)(sum + result[m] * pow);
+            }
+
+            if (x == sum) {
+                System.out.println("true");
+                return true;
+            }
+            System.out.println("false");
+            return false;
+        } else {
+            System.out.println("false");
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution12 solution12 = new Solution12();
+        solution12.isPalindrome(121232121);
     }
 }
 
