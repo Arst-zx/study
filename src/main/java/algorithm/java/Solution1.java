1fa1b12b7833e57665695193e92119b3a447351b
zhangx511 <zhangx511@chinaunicom.cn>
2020-09-25 15:09:05 +0800

    leetcode算法

--- src/main/java/algorithm/java/Solution1.java
+++ src/main/java/algorithm/java/Solution1.java
@@ -1,5 +1,7 @@
 package algorithm.java;
 
+import java.util.Stack;
+
 /**
  * @author: 章鑫
  * @Email: zhangx511@chinaunicom.cn
@@ -89,6 +91,9 @@ class Solutionl {
     }
 }
 
+/**
+ * 回文数
+ */
 class Solution12 {
     public boolean isPalindrome(int x) {
         if (x >=0) {
@@ -126,4 +131,239 @@ class Solution12 {
     }
 }
 
+/**
+ * 罗马数字转整数
+ */
+class Solutionw {
+    public int romanToInt(String s) {
+        s = s.replaceAll("IV", "a");
+        s = s.replaceAll("IX", "b");
+        s = s.replaceAll("XL", "c");
+        s = s.replaceAll("XC", "d");
+        s = s.replaceAll("CD", "e");
+        s = s.replaceAll("CM", "f");
+        String[] sum = s.split("");
+        int result = 0;
+        for (int i=0; i<sum.length; i++) {
+            if ("I".equals(sum[i])) {
+                result = result + 1;
+            }
 
+            if ("V".equals(sum[i])) {
+                result = result + 5;
+            }
+
+            if ("X".equals(sum[i])) {
+                result = result + 10;
+            }
+
+            if ("L".equals(sum[i])) {
+                result = result + 50;
+            }
+
+            if ("C".equals(sum[i])) {
+                result = result + 100;
+            }
+
+            if ("D".equals(sum[i])) {
+                result = result + 500;
+            }
+
+            if ("M".equals(sum[i])) {
+                result = result + 1000;
+            }
+
+            if ("a".equals(sum[i])) {
+                result = result + 4;
+            }
+
+            if ("b".equals(sum[i])) {
+                result = result + 9;
+            }
+
+            if ("c".equals(sum[i])) {
+                result = result + 40;
+            }
+
+            if ("d".equals(sum[i])) {
+                result = result + 90;
+            }
+
+            if ("e".equals(sum[i])) {
+                result = result + 400;
+            }
+
+            if ("f".equals(sum[i])) {
+                result = result + 900;
+            }
+        }
+        System.out.println(result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solutionw solutionw = new Solutionw();
+        solutionw.romanToInt("MCMXCIV");
+    }
+}
+
+/**
+ * 罗马数字转整数
+ */
+class Solutione {
+    public int romanToInt(String s) {
+        s = s.replaceAll("IV", "a");
+        s = s.replaceAll("IX", "b");
+        s = s.replaceAll("XL", "c");
+        s = s.replaceAll("XC", "d");
+        s = s.replaceAll("CD", "e");
+        s = s.replaceAll("CM", "f");
+        String[] sum = s.split("");
+        int result = 0;
+        for (int i=0; i<sum.length; i++) {
+            switch (sum[i]) {
+                case "I":
+                    result = result + 1;
+                    break;
+                case "V":
+                    result = result + 5;
+                    break;
+                case "X":
+                    result = result + 10;
+                    break;
+                case "L":
+                    result = result + 50;
+                    break;
+                case "C":
+                    result = result + 100;
+                    break;
+                case "D":
+                    result = result + 500;
+                    break;
+                case "M":
+                    result = result + 1000;
+                    break;
+                case "a":
+                    result = result + 4;
+                    break;
+                case "b":
+                    result = result + 9;
+                    break;
+                case "c":
+                    result = result + 40;
+                    break;
+                case "d":
+                    result = result + 90;
+                    break;
+                case "e":
+                    result = result + 400;
+                    break;
+                case "f":
+                    result = result + 900;
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.println(result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solutione solution = new Solutione();
+        solution.romanToInt("MCMXCIV");
+    }
+}
+
+/**
+ * 最长公共前缀
+ */
+class Solutionr {
+    public String longestCommonPrefix(String[] strs) {
+
+        if (strs.length < 1) {
+            return "";
+        }
+
+        if (strs.length == 1) {
+            return strs[0];
+        }
+
+        String samestr;
+        String result = "";
+        for (int m=1; m<=strs[0].length(); m++) {
+            samestr = strs[0].substring(0,m);
+            boolean flag = false;
+            for (int i=1; i<strs.length; i++) {
+                if (!strs[i].startsWith(samestr)) {
+                    samestr = strs[0].substring(0,m-1);
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (flag) {
+                if (samestr.length()>result.length()) {
+                    result = samestr;
+                }
+                break;
+            } else {
+                if (samestr.length()>result.length()) {
+                    result = samestr;
+                }
+            }
+        }
+        System.out.println(result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solutionr solutionr = new Solutionr();
+        String[] ttt = {"ca","a"};
+        solutionr.longestCommonPrefix(ttt);
+    }
+}
+
+/**
+ * 有效的括号
+ */
+class Solutioni {
+    public boolean isValid(String s) {
+
+        if (s.length()%2 == 1) {
+            System.out.println("false");
+            return false;
+        }
+
+        String[] chars = s.split("");
+        Stack<String> stack = new Stack<>();
+        stack.push(chars[0]);
+        for (int i=1; i<chars.length; i++) {
+
+            if (stack.isEmpty()) {
+                stack.push(chars[i]);
+            } else if (
+                    ("(".equals(stack.peek()) && ")".equals(chars[i])) ||
+                    ("[".equals(stack.peek()) && "]".equals(chars[i])) ||
+                    ("{".equals(stack.peek()) && "}".equals(chars[i]))
+            ){
+                stack.pop();
+            } else {
+                stack.push(chars[i]);
+            }
+        }
+
+        if (stack.isEmpty()) {
+            System.out.println("true");
+            return true;
+        } else {
+            System.out.println("false");
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solutioni solutioni = new Solutioni();
+        solutioni.isValid("{}[{}]((){})(){}");
+    }
+}
