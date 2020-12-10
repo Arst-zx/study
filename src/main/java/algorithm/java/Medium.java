380599a947d51e048c745fdb77ad0835ad49a303
zhangx511 <zhangx511@chinaunicom.cn>
2020-12-10 10:33:37 +0800

    算法学习

--- src/main/java/algorithm/java/Medium.java
+++ src/main/java/algorithm/java/Medium.java
@@ -496,3 +496,48 @@ class Medium8 {
         System.out.println(nums);
     }
 }
+
+class Medium9 {
+//    public int search(int[] nums, int target) {
+//        for (int i = 0; i < nums.length; i++) {
+//            if (nums[i] == target) {
+//                return i;
+//            }
+//        }
+//        return -1;
+//    }
+
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int m = (l + r)/2;
+            if (nums[m] == target) {
+                return m;
+            }
+            if (nums[m] >= nums[l]) {
+                if (target >= nums[l] && target < nums[m]) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } else {
+                if (target > nums[m] && target <= nums[r]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Medium9 medium9 = new Medium9();
+        int result = medium9.search(new int[] {14,15,16,17,0,1,2,3,4,5,6}, 15);
+        System.out.println(result);
+    }
+}
