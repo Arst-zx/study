71ee44f79b94b65bede9d032dbbd4d9838e17f4c
zhangx511 <zhangx511@chinaunicom.cn>
2021-03-04 11:17:46 +0800

    算法

--- src/main/java/algorithm/java/Easy.java
+++ src/main/java/algorithm/java/Easy.java
@@ -48,7 +48,6 @@ class TreeNode {
  */
 public class Easy {
     public int[] twoSum(int[] nums, int target) {
-
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[j] == target - nums[i]) {
